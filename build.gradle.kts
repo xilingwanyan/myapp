ac2cbe771bbcc45434a5cb37ece68d5ab0d0279f
xilingwanyan <xilingwanyan@163.com>
2025-08-20 10:31:13 +0800

    提交

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@
 plugins {
     id("com.android.application") version "8.5.1" apply false
     id("com.android.library") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false     
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
 }
 
 tasks.register<Delete>("clean") {
